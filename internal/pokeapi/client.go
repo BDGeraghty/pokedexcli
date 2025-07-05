ba059b2176241a6cee9b95c2d475792ad1f78ac2
KOYSAS <bdgeraghty@gmail.com>
2025-07-05 10:51:27 -0400

    pokecache

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,18 +3,22 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	//"github.com/bootdotdev/pokedexcli/internal/pokecache"
+	"github.com/bootdotdev/pokedexcli/internal/pokecache" // Client -
 )
 
-// Client -
 type Client struct {
 	httpClient http.Client
+	cache      *pokecache.Cache
 }
 
 // NewClient -
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout time.Duration, cacheTTL time.Duration) Client {
 	return Client{
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
+		cache: pokecache.NewCache(cacheTTL), // Default cache TTL
 	}
 }
--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 )
 
 func main() {
-	pokeClient := pokeapi.NewClient(5 * time.Second)
+	pokeClient := pokeapi.NewClient(
+		time.Second*10, // HTTP timeout
+		time.Minute*5,   // Cache TTL
+	)
 	cfg := &config{
 		pokeapiClient: pokeClient,
 	}
